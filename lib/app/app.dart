76e657f433c266da27701283b0066764fc65c08d
Bacon <matheus.camarinha.duarte@gmail.com>
2025-05-19 19:20:09 -0300

    Separation of theme-related functions

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,65 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FitSwimApp extends StatelessWidget {
-  const FitSwimApp({super.key});
+  FitSwimApp({super.key});
+
+  List<Widget> listOfTile = [
+    ListTile(
+      title: Text("Aluno A"),
+      subtitle: Text("Touca Azul"),
+      leading: Icon(Icons.person),
+    ),
+    ListTile(
+      title: Text("Aluno B"),
+      subtitle: Text("Touca Preta"),
+      leading: Icon(Icons.person),
+    ),
+    ListTile(
+      title: Text("Aluno C"),
+      subtitle: Text("Touca Roxa"),
+      leading: Icon(Icons.person),
+    ),
+    ListTile(
+      title: Text("Aluno D"),
+      subtitle: Text("Touca Vermelha"),
+      leading: Icon(Icons.person),
+    ),
+    ListTile(
+      title: Text("Aluno E"),
+      subtitle: Text("Touca Laranja"),
+      leading: Icon(Icons.person),
+    ),
+    ListTile(
+      title: Text("Aluno F"),
+      subtitle: Text("Touca Verde"),
+      leading: Icon(Icons.person),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Text("Fit Swim"),),
+      appBar: AppBar(
+        title: Text("Fit Swim"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+                width: double.infinity,
+                height: 500,
+                child: GridView(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4),
+                    children: listOfTile))
+          ],
+        ),
+      ),
     );
   }
 }
