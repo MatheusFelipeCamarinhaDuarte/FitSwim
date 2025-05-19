a503f2e26ec66b573b14378cc87cf4cda99f2dee
Bacon <matheus.camarinha.duarte@gmail.com>
2025-05-19 16:01:09 -0300

    Delete the initial pages and creation of the temporary placeholder pages

---
+++ lib/app/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FitSwimApp extends StatelessWidget {
+  const FitSwimApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(child: Text("Fit Swim"),),
+    );
+  }
+}
